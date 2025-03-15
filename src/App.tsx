61868d555d76e5a9a36085c9ae79ff1f6312f7aa
asritha-lolla <asrithalolla@gmail.com>
2025-03-15 12:06:06 +0530

    Project initial setup done

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@mui/material"
+
+import './assets/styles/global.css'
+
+
+const App = () => {
+  return (
+    <div>
+      <Button>HELLO</Button>
+      HELLOOOO
+    </div>
+  )
+}
+
+export default App
